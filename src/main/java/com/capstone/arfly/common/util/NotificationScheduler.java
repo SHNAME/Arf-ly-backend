af0402a85958e122d9fb25c670f84d48f595f669
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-13 14:01:43 +0900

    refactor: 약 푸시 알람 일괄 발송 로직 병목 현상 개선
    - 기존 방식:알람 대상자들을 한번에 조회하여 FCM 단건 전송 API를 통해 1개씩 전송하는 방식으로 처리
    - 문제: 루프 내 단건 발송 방식을 통해 조회된 대상자 수만큼 네트워크
      요청이 발생하여 발송 시 지연시간 발생, 대상자 조회를 한번의 Query로
      처리하기 때문에 대상자가 매우 많을 경우 메모리 부하 발생 가능
    - 해결:스케줄러 내 대량 데이터 조회 시 메모리 과부하 방지를 위해 Slice 페이징 기법 도입, SendEach API를 통해 한번에 500개의 메세지를 일괄 전송함으로써 네트워크 요청을 크게 줄임

--- src/main/java/com/capstone/arfly/common/util/NotificationScheduler.java
+++ src/main/java/com/capstone/arfly/common/util/NotificationScheduler.java
@@ -9,8 +9,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,38 +31,48 @@ public class NotificationScheduler {
     @Scheduled(cron = "0 * * * * *")
     public void sendScheduledNotifications() {
         log.info("약 알림 발송 스케줄러 작동");
-        // 알림 발송 대상자 조회
         LocalTime now = LocalTime.now();
         LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
-        List<MedicationAlarmDto> reminders = medicationReminderRepository.findPendingNotifications(now,
-                startOfDay);
-        if(reminders.isEmpty()) return;
+        int pageNumber = 0;
+        int pageSize = 500; // FCM 일괄 전송은 최대 500개까지만 가능
+        while (true) {
+            // DB에서 500개씩 끊어서 조회
+            Slice<MedicationAlarmDto> reminderSlice = medicationReminderRepository.findPendingNotifications(
+                    now, startOfDay, PageRequest.of(pageNumber, pageSize)
+            );
+            if (reminderSlice.isEmpty()) {
+                break;
+            }
+            List<MedicationAlarmDto> reminders = reminderSlice.getContent();
 
-        // 알림 발송 및 실패한 토큰 ID 수집
-        List<Long> failedFcmTokenIds = firebaseService.sendAllNotifications(reminders);
+            // 알림 발송 및 실패한 토큰 ID 수집
+            Set<Long> failedFcmTokenIds = firebaseService.sendAllNotifications(reminders);
 
-        // 발송에 성공한 FCM Token ID를 추출
-        List<Long> successFcmTokenIds = reminders.stream()
-                .map(MedicationAlarmDto::fcmTokenId)
-                .filter(tokenId ->!failedFcmTokenIds.contains(tokenId))
-                .distinct()
-                .toList();
-        // 대상자 알림 전부 업데이트(실패 알람 무한 재시도 막기 위함)
-        List<Long> allMedicationReminderIds = reminders.stream()
-                .map(MedicationAlarmDto::medicationId)
-                .distinct()
-                .toList();
+            // 발송에 성공한 FCM Token ID를 추출
+            List<Long> successFcmTokenIds = reminders.stream()
+                    .map(MedicationAlarmDto::fcmTokenId)
+                    .filter(tokenId -> !failedFcmTokenIds.contains(tokenId))
+                    .distinct()
+                    .toList();
+            // 대상자 알림 전부 업데이트(실패 알람 무한 재시도 막기 위함)
+            List<Long> allMedicationReminderIds = reminders.stream()
+                    .map(MedicationAlarmDto::medicationId)
+                    .distinct()
+                    .toList();
 
-        //알람 결과 및 FCM Token 업데이트
-        notificationService.updateAlarmResults(successFcmTokenIds,failedFcmTokenIds,allMedicationReminderIds);
+            List<Long> failedFcmTokenList = failedFcmTokenIds.stream().toList();
+            //알람 결과 및 FCM Token DB 업데이트
+            notificationService.updateAlarmResults(successFcmTokenIds, failedFcmTokenList, allMedicationReminderIds);
+            if(!reminderSlice.hasNext()) break;
 
+        }
         log.info("약 알림 발송 스케줄러 종료");
     }
 
     // 매일 오전 3시에 한달동안 사용하지 않은 Fcm Token 제거
     @Transactional
     @Scheduled(cron = "0 0 3 * * *")
-    public void cleanUpInactiveTokens(){
+    public void cleanUpInactiveTokens() {
         log.info("휴먼 FCM 토큰 정리 시작");
         LocalDateTime oneMonthAgo = LocalDateTime.now().minusMonths(1);
         fcmTokenRepository.deleteByLastUsedAtBefore(oneMonthAgo);
