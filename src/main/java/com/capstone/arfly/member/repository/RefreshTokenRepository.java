f359a531397a011bc3843fa0de70732c26b17cdd
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-28 11:31:11 +0900

    feat:토큰의 만료일이 현재 날짜보다 작은 토큰만 삭제하기 위한 Query 작성

--- src/main/java/com/capstone/arfly/member/repository/RefreshTokenRepository.java
+++ src/main/java/com/capstone/arfly/member/repository/RefreshTokenRepository.java
@@ -4,11 +4,18 @@ import com.capstone.arfly.member.domain.RefreshToken;
 import java.util.Date;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
     Optional<RefreshToken> findByToken(String refreshToken);
 
-    void deleteByExpiredAtBefore(Date date);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM RefreshToken r WHERE r.expiredAt < :now")
+    void deleteByExpiredAtBefore(@Param("now") Date date);
 }
