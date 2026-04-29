137590a750bf831194052ac24599e8b8664d1bd9
White_DeepDive <tlgud119@yu.ac.kr>
2026-04-29 13:10:01 +0900

    fix(Pet): 나의 반려 동물 목록 조회에서 발생하는 N+1 문제 해결
    - 문제:  기존 findAllByMemberId는 조건에 맞는 Pet만 조회하고 Pet에 포함된 프로필 이미지는 조회하지 않음
    - 반려 동물 목록 조회에 N개 반려동물에 대한 프로필 이미지 접근시 추가 SELECT 쿼리 발생(N+1)
    - 해결 JPQL 내 FETCH JOIN을 사용하여 PET을 가져올 때 이미지도 함께 가져오도록 변경

--- src/main/java/com/capstone/arfly/pet/repository/PetRepository.java
+++ src/main/java/com/capstone/arfly/pet/repository/PetRepository.java
@@ -2,6 +2,8 @@ package com.capstone.arfly.pet.repository;
 
 import com.capstone.arfly.pet.domain.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,9 @@ import java.util.List;
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
 
-    List<Pet> findAllByMemberId(Long memberId);
+    @Query("""
+    SELECT p FROM Pet  p LEFT JOIN FETCH p.profileImage WHERE p.member.id = :memberId 
+    """)
+    List<Pet> findAllByMemberId(@Param("memberId") Long memberId);
 
 }
