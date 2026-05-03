a8ea39619d2c8d39630ea41cf5d46730bafb7984
White_DeepDive <tlgud119@yu.ac.kr>
2026-05-03 13:09:53 +0900

    refactor: 리프레시 토큰 저장소 DB에서 Redis로 교체

--- src/main/java/com/capstone/arfly/common/constant/RedisConstant.java
+++ src/main/java/com/capstone/arfly/common/constant/RedisConstant.java
@@ -3,4 +3,5 @@ package com.capstone.arfly.common.constant;
 public class RedisConstant {
     public static final String SEARCH_HISTORY_PREFIX = "search:history:";
     public static final int MAX_SEARCH_HISTORY_SIZE = 10;
+    public static final String REFRESH_TOKEN_PREFIX = "refresh:token:";
 }
